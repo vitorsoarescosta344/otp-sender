0c3fe90c4022a7bc8b29f103e6182266966f0227
Victor Soares <vitorsoarescosta344@gmail.com>
2022-07-12 16:58:12 -0300

    otp model

---
+++ src/model/otp.model.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from "mongoose";
+
+const otpSchema = new Schema(
+  {
+    otp: { type: String, required: true },
+    verified: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const OTP = mongoose.model("OTP", otpSchema);
+
+export default OTP;
